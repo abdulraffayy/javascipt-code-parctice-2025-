712648dc5e410029d03897b13323e2dd8192fc11
Abdul Rafay <rajpootrafay96@gmail.com>
2025-11-24 16:38:00 +0500

    �Chapter 1: Variables & Declarations

---
+++ firstpartrevisonsection/script.js
@@ -0,0 +1,28 @@
+//  let, and const – Line-by-Line Comparison
+
+var a = rafay;
+var b = "hello";
+var c = true;
+var d = 45;
+var e = null;
+
+  
+console.log(a);
+
+let hamza = "this is let variable";
+console.log(hamza);
+
+
+const pi = 3.14;
+console.log(pi);
+
+console.log("Hello, World!");
+
+
+
+// declarations and initializations
+
+
+
+var alishah;   // decleare karna 
+var alishah = "hello";  // declare or initialize karna 
