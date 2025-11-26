795f1faac8a7e6b196b87b97e39483a823977d5d
Abdul Rafay <rajpootrafay96@gmail.com>
2025-11-26 18:33:10 +0500

    Chapter 1: Variables & Declarations chapter completed

--- firstpartrevisonsection/script.js
+++ firstpartrevisonsection/script.js
@@ -34,3 +34,110 @@ var a = 20;  // re-declaration is allowed
 // const example
 const pi = 3.14;
 console.log(pi);
+
+
+
+// Scope (global , block, Functional
+
+
+
+
+// fucntional scoped example 
+
+
+function testVar() {
+    var x = 1;      
+
+}
+
+
+testVar();
+
+
+// gloabl scoped example
+
+var y = 10;   // global scope
+
+
+consle.log(y);
+
+
+
+// block scoped example
+
+    function Data (){
+        let z = 5;   // block scope
+    }
+
+
+    console.log(z);  // error  z is not defined
+
+
+
+// Reassignment, redeclaration
+
+var c = 30;
+c = 40;
+
+
+// let d = 50
+// d = 60;  // reassignment is allowed
+
+
+
+
+
+// Temperal Dead Zone (TDZ)
+
+console.log(rafay);  // undefined
+let rafay = "hello"; 
+
+
+
+// hositing imapact per type 
+
+console.log(a);  // undefined
+
+a = 10;
+
+
+// paractice question
+
+
+// provblem 1
+console.log(nm);
+
+var nm = "rafay";   // answer is undefined 
+
+
+// problem 2
+
+console.log(age);
+let age = 25;      // answer is Uncaught ReferenceError: Cannot access 'age' before initialization
+
+
+
+// PROBLEM 3
+
+var x = 1 ; // gloabal
+
+{
+    var x = 2;    // gloabal and var is not respected 
+}
+
+console.log(x);    // answer is 2
+
+
+
+
+// PROBLEM 4
+
+
+let rr = 10;
+
+{
+    let a = 20;   // block scope
+    console.log("inside:", a);   // answer is 20
+}
+
+console.log("outside:", rr);   // answer is 10
