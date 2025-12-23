2df73d2be8851fcfe30782283c83048b689baf92
Abdul Rafay <rajpootrafay96@gmail.com>
2025-12-23 15:55:29 +0500

    chapter 6 Function will be completed the paractice files

--- Chapter 6 Function/paractice.js
+++ Chapter 6 Function/paractice.js
@@ -69,6 +69,148 @@ console.log(pureFunc(5));
 
 })();
 
-// console.log(password); // This will throw an error because password is not defined outside the IIFE.
+// // console.log(password); // This will throw an error because password is not defined outside the IIFE.
 
-console.log(password);
+// console.log(password);
+
+
+
+// chat gpt function related question .
+
+function SayHe() {
+    console.log("Hello JavaScript!")
+}
+
+SayHe();
+
+
+
+// problem 2 Ek function addNumbers banao
+
+// 2 parameters le (a, b)
+
+// Dono ka sum return kare
+
+
+function AddingNumber(a, b) {
+    console.log(a + b);
+    
+}
+
+AddingNumber(3, 5); // 8
+
+
+
+
+// problem 3  Ek function square banao
+
+// Ek number le
+
+// Us number ka square return kare
+
+function squareNumber (num) {
+    return console.log(num * num);
+
+}
+
+squareNumber(4); // 16
+squareNumber(5); // 25
+
+// problem 4 
+// Ek function isEven banao
+
+// Ek number le
+
+// Agar number even ho to true return kare
+
+// Warna false
+
+function isEven(num) {
+    if(num % 2 === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+console.log(isEven(4)); // true
+console.log(isEven(7)); // false
+
+
+
+// pronlem 5
+
+// Ek function findLargest banao
+
+// 3 numbers parameters mein le
+
+// Sab se bara number return kare
+
+function findLarget(a, b, c) {
+    if(a >= b && a >= c){
+        console.log(a);
+    }
+    else if(b >= a && b >= c){
+        console.log(b);
+    }
+    else{
+        console.log(c);
+    }
+
+}
+
+findLarget(3, 2, 5); // 7
+
+
+
+// problem 6
+// Ek function countVowels banao
+
+// Ek string le
+
+// Us mein jitne vowels hain unki count return kare
+// (vowels: a, e, i, o, u)   
+
+
+
+function countVowels(str) {
+    let count = 0;
+    let vowels = "aeiouAEIOU";
+    for (let char of str) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countVowels("hello")); // 2
+console.log(countVowels("javascript")); // 3
+
+
+
+// problem 7    
+// Ek function reverseAString banao
+
+// Ek string le
+function reverseAString(str) {
+    return str.split("").reverse().join("");
+   
+}
+console.log(reverseAString("hello")); // "olleh"
+console.log(reverseAString("JavaScript")); // "tpircSavaJ"
+
+
+
+
+// one more approach to reverse a string
+
+function reverseString(str) {
+    let reversed = "";
+    for (let i = str.length - 1; i>=0; i--){
+        reversed += str[i];
+    }
+    return reversed;
+}
+
+reverseString("jazzcash"); // hsaczzaj
+console.log(reverseString("jazzcash"));
